f7423380471c77d2df62688700f992efe761b970
christian <kanungachiristian27@gmail.com>
2026-02-04 14:41:55 +0300

    Creation of the server and link to the registration form

---
+++ server.js
@@ -0,0 +1,56 @@
+
+
+const express = require('express');
+const mysql = require('mysql2');
+require('dotenv').config();
+
+ 
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//create the vdatabase connection to the database
+
+const db = mysql.createPool({  
+    host: 'localhost',
+    user: 'phone_user',       
+    password: 'userpassword', 
+    database: 'phone_shop', 
+
+    connectionLimit: 100,
+    waitForConnections: true,
+    queueLimit: 0,
+    port: 3307
+});
+
+
+ //test the connection
+ db.getConnection((err,connection)=>{
+    if(err){
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MySQL database');
+    connection.release();
+ });
+
+//the root fo the registration
+app.post('/register', (req, res) => {
+    const { username,email,password} = req.body;
+
+    const sqlInsert = "INSERT INTO users (username,email,password) VALUES (?,?,?)";
+    db.query(sqlInsert, [username,email,password], (err, result) => {
+        if (err) {
+            console.error('Error inserting data into the database:', err);
+            res.status(500).send('Error registering user');
+        } else {
+            res.status(200).send('User registered successfully');
+        }
+    }); 
+});
+
+const port = 3000;
+
+app.listen(port,()=>{
+    console.log('Now server is running at port ' + port);
+});
